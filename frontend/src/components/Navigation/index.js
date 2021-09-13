faf02be78cec0d11c917051bdd7ec600a02d7947
Tyler Boromeo <boromeotyler@gmail.com>
2021-09-12 19:11:26 -0700

    Added links to nav bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,15 +1,33 @@
-import ProfileButton from "./ProfileButton";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
 
-const Navigation = () => {
-  const session = useSelector(state => state.session.user);
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
 
-  if (session) {
-    return <ProfileButton />
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
   }
 
   return (
-    null
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
   );
 }
 
