4c75fc9aa5499b5e4636e9d398e8bedaeb0dff0f
Tyler Boromeo <boromeotyler@gmail.com>
2021-09-14 16:00:25 -0700

    Added spotReducer, thunker, and action creator

--- frontend/src/components/SpotsPage/index.js
+++ frontend/src/components/SpotsPage/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SpotCard from './SpotCard';
 
 const SpotsPage = () => {
+
   return (
     <>
       <h2>Spots page</h2>
---
+++ frontend/src/store/spots.js
@@ -0,0 +1,28 @@
+const SET_SPOTS = 'spots/setSpots';
+
+const setSpots = (spots) => {
+  return {
+    type: SET_SPOTS,
+    payload: spots,
+  };
+};
+
+export const getSpots = () => async dispatch => {
+  const response = await fetch('/api/spots');
+  const data = await response.json();
+  dispatch(setSpots(data.spots));
+  return response;
+}
+
+const spotsReducer = (state = [], action) => {
+  let newState;
+  switch (action.type) {
+    case SET_SPOTS:
+      newState = action.payload.map(spot => spot);
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default spotsReducer;
