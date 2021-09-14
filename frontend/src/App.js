3eec23594aa5a18ebc40515e238d46330826bf6d
Tyler Boromeo <boromeotyler@gmail.com>
2021-09-14 09:36:06 -0700

    added btn on home page for easy navigation during development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -16,6 +16,12 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path='/'>
+            <NavLink to='/s/home'>spots</NavLink>
+          </Route>
+          <Route path='/s/home'>
+
+          </Route>
         </Switch>
       )}
     </>
