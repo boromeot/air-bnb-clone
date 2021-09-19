6f12f5834cc38c81088cc609b36e406c2d9bac5c
Tyler Boromeo <boromeotyler@gmail.com>
2021-09-18 19:15:55 -0700

    header styling

--- frontend/src/components/SpotsPage/index.js
+++ frontend/src/components/SpotsPage/index.js
@@ -3,6 +3,7 @@ import SpotCard from './SpotCard';
 import * as spotsActions from '../../store/spots';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import './SpotsPage.css';
 
 const SpotsPage = () => {
   const dispatch = useDispatch();
@@ -13,14 +14,15 @@ const SpotsPage = () => {
   }, [dispatch])
 
   return (
-    <>
-      <h2 className='spots-header'>Spots page</h2>
+    <div className='spots-page-container'>
+      <div className='spots-header'>
+      </div>
       <div className='spots-container'>
         {spots.map(({id, city, state, price, Images}) => (
           <SpotCard id={id} city={city} state={state} price={price} images={Images} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
