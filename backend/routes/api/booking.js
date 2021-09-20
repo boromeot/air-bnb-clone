17e2af7d107589cab213ee9d46ed11a127230b75
Tyler Boromeo <boromeotyler@gmail.com>
2021-09-19 22:33:46 -0700

    created bookings get route

--- backend/routes/api/booking.js
+++ backend/routes/api/booking.js
@@ -3,6 +3,19 @@ const asyncHandler = require('express-async-handler');
 const { Booking } = require('../../db/models');
 const router = express.Router();
 
+router.get('/user/:userId', asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  const bookings = await Booking.findAll({
+    where: {
+      userId
+    },
+  })
+
+  return res.json({
+    bookings,
+  });
+}));
+
 router.post('/', asyncHandler(async (req, res) => {
   const {userId, spotId, startDate, endDate} = req.body;
   const booking = await Booking.create({
