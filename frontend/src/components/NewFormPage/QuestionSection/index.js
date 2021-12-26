b341863c2f34e841041c68b1380f4534bc29be73
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-26 09:20:07 -0800

    added airbnb logo to page

--- frontend/src/components/NewFormPage/QuestionSection/index.js
+++ frontend/src/components/NewFormPage/QuestionSection/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import AirbnbLogo from "../../SVGs/AirbnbLogo";
 
 const QuestionSection = ({ question }) => {
   return (
     <div className="flex flex-column w50p h100vh fixed justify-center"
       style={{background: 'rgba(0,0,0,0) url("https://a0.muscache.com/im/pictures/Hosting/Gradient/original/4abb91ab-2c70-46d7-80a5-9c83ef0221ef.png") no-repeat scroll 0% 0% / 100% 100%'}}
     >
-
+      <div className="fixed"
+        style={{top: '32px', left: '48px'}}
+      >
+        <NavLink to='#'>
+          <AirbnbLogo className='icon-32' viewbox='0 0 32 32' />
+        </NavLink>
+      </div>
     </div>
   )
 }
