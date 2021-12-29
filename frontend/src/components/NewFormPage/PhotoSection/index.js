a0f8fd4b167c5a236e9dcca40cd1efdd1fd9ba8c
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-29 14:18:55 -0800

    added mapboxgl

---
+++ frontend/src/components/NewFormPage/PhotoSection/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PhotoSection = () => {
+  return (
+    <div id="FMP-target" className="FMP-target mz--24" style={{maxWidth: '500px'}}>
+      
+    </div>
+  )
+}
+
+export default PhotoSection;
