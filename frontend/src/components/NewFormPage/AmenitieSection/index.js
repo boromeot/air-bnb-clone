120feb0ccbd8e1fb2b5b1e325307415c293ccb1f
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-31 16:13:08 -0800

    added hottub icon

--- frontend/src/components/NewFormPage/AmenitieSection/index.js
+++ frontend/src/components/NewFormPage/AmenitieSection/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AmenitieOption from "./AmenitieOption";
 import Pool from "../../SVGs/Pool";
+import HotTub from "../../SVGs/HotTub";
 
 const AmenitieSection = ({ formData, setFormData }) => {
   return (
@@ -9,7 +10,7 @@ const AmenitieSection = ({ formData, setFormData }) => {
         <h2 className="font-size--22 pl--8 font-weight--600" style={{marginBottom: '15px'}}>Do you have any standout amenities?</h2>
         <div className="flex flex-row-wrap mt4">
           <AmenitieOption amenitie='Pool' formData={formData} setFormData={setFormData} svg={<Pool className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='Hot tub'formData={formData} setFormData={setFormData} svg={null}/>
+          <AmenitieOption amenitie='Hot tub'formData={formData} setFormData={setFormData} svg={<HotTub className='icon-52' viewBox='0 0 100 100' />} />
           <AmenitieOption amenitie='Patio'formData={formData} setFormData={setFormData} svg={null}/>
           <AmenitieOption amenitie='BBQ grill'formData={formData} setFormData={setFormData} svg={null}/>
           <AmenitieOption amenitie='Fire pit'formData={formData} setFormData={setFormData} svg={null}/>
