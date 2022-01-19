097ba15eb5ef82486dc04713587fdc8d90252a0c
Tyler Boromeo <boromeotyler@gmail.com>
2022-01-18 23:35:25 -0800

    updated a few svgs

--- frontend/src/components/NewFormPage/AmenitieSection/index.js
+++ frontend/src/components/NewFormPage/AmenitieSection/index.js
@@ -15,20 +15,26 @@ const AmenitieSection = ({ setFormData, formAmenities }) => {
 
   const [selected, setSelected] = useState(formAmenities);
 
+  const svgProps = {
+    className: 'icon-52',
+    viewBox: '0 0 100 100',
+    transform: 'translate(-31, -30) scale(2, 2)'
+  }
+
   return (
     <div id="FMP-target" className="FMP-target flex flex-column">
       <div className="mz--24" style={{maxWidth: '656px'}}>
         <h2 className="font-size--22 pl--8 font-weight--600" style={{marginBottom: '15px'}}>Do you have any standout amenities?</h2>
         <div className="flex flex-row-wrap mt4">
-          <AmenitieOption amenitie='Pool' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Pool className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='Hot tub'selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<HotTub className='icon-52' viewBox='0 0 100 100' />} />
-          <AmenitieOption amenitie='Patio' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Patio className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='BBQ grill' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Grill className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='Fire pit' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<FirePit className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='Pool table' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<PoolTable className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='Indoor fireplace'selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<FirePlace className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='Outdoor dining area' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<DiningArea className='icon-52' viewBox='0 0 100 100' />}/>
-          <AmenitieOption amenitie='Exercise equipment' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Dumbell className='icon-52' viewBox='0 0 100 100' />}/>
+          <AmenitieOption amenitie='Pool' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Pool {...svgProps} />}/>
+          <AmenitieOption amenitie='Hot tub'selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<HotTub {...svgProps} />} />
+          <AmenitieOption amenitie='Patio' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Patio {...svgProps} />}/>
+          <AmenitieOption amenitie='BBQ grill' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Grill {...svgProps} />}/>
+          <AmenitieOption amenitie='Fire pit' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<FirePit {...svgProps} />}/>
+          <AmenitieOption amenitie='Pool table' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<PoolTable {...svgProps} />}/>
+          <AmenitieOption amenitie='Indoor fireplace'selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<FirePlace {...svgProps} />}/>
+          <AmenitieOption amenitie='Outdoor dining area' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<DiningArea {...svgProps} />}/>
+          <AmenitieOption amenitie='Exercise equipment' selected={selected} setSelected={setSelected} setFormData={setFormData} svg={<Dumbell {...svgProps} />}/>
         </div>
       </div>
     </div>
--- frontend/src/components/SpotPage/SpotAmenities/index.js
+++ frontend/src/components/SpotPage/SpotAmenities/index.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Amenity from "./Amenity";
 import Wifi from "../../SVGs/Wifi";
+import Pool from "../../SVGs/Pool";
+import HotTub from "../../SVGs/HotTub";
+import Grill from "../../SVGs/Grill";
 
 const SpotAmenities = ({ amenities }) => {
+  const props = {
+    className: 'icon-24',
+    viewBox: '0 0 32 32',
+    transform: 'translate(-23, -23)'
+  }
+
   return (
     <>
       <div className="mb4">
         <h2 className="font-size--22">What this place offers</h2>
       </div>
       <div className="flex flex-wrap">
-        <Amenity svg={<Wifi />} desc='Fast wifi' />
-        <Amenity svg={<Wifi />} desc='Fast wifi' />
-        <Amenity svg={<Wifi />} desc='Fast wifi' />
-        <Amenity svg={<Wifi />} desc='Fast wifi' />
+        <Amenity svg={<Wifi {...props} />} desc='Fast wifi' />
+        <Amenity svg={<Pool {...props} /> } desc='Pool' />
+        <Amenity svg={<HotTub {...props} />} desc='Hot tub' />
+        <Amenity svg={<Grill {...props} />} desc='Grill' />
         <Amenity svg={<Wifi />} desc='Fast wifi' />
       </div>
     </>
