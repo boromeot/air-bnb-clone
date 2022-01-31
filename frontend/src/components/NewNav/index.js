bdffd41221cc0562be6d228c2a9f68c0b89e738a
Tyler Boromeo <boromeotyler@gmail.com>
2022-01-31 10:45:24 -0800

    fixed some spacing issues

--- frontend/src/components/NewNav/index.js
+++ frontend/src/components/NewNav/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import AirbnbNavLogo from "../SVGs/AirbnbNavLogo";
+import Globe from "../SVGs/Globe";
 import './Nav.css';
 
 const Nav = () => {
   return (
     <header className="new-nav">
       <div className="nav-container">
-        <a href='/' className="nav-logo">
-          <div>
+        <div className="nav-logo-container">
+          <a href='/' className="nav-logo">
             <AirbnbNavLogo style={{height: '32px', width: '102px', fill: 'white'}} />
-          </div>
-        </a>
-        <div></div>
+          </a>
+        </div>
+        <div className="nav-search"></div>
         <div className="nav-buttons-container">
           <div className="nav-host-container">
-            <a href='/' className="nav-host-link">Switch to hosting</a>
+            <a href='/' className="nav-host-link">
+              <div className="nav-switch-hosting">Switch to hosting</div>
+            </a>
+            <div className="nav-globe">
+              <button className="nav-globe-button"><Globe /></button>
+            </div>
           </div>
         </div>
       </div>
