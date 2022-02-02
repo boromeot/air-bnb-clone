46ea3f2f3eb5fcc9039796b5c052316694203ed5
Tyler Boromeo <boromeotyler@gmail.com>
2022-02-02 11:46:12 -0800

    added right chevron

---
+++ frontend/src/components/SVGs/RightChevron.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightChevron = ({ className, viewBox }) => {
+  return (
+    <svg className={ className } viewBox={ viewBox }>
+      <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd" />
+    </svg>
+  )
+}
+
+export default RightChevron;
