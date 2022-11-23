32e1d8baded3e05f038b9af76ca4dffb735170eb
boromeot <boromeotyler@gmail.com>
2022-11-23 03:41:42 -0800

    removed mapbox for now

--- frontend/src/components/NewFormPage/AddressSection/index.js
+++ frontend/src/components/NewFormPage/AddressSection/index.js
@@ -1,37 +1,37 @@
 // import 'mapbox-gl/dist/mapbox-gl.css'
 // import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import React, { useState, useRef, useCallback } from 'react'
-import MapGL from 'react-map-gl'
-import Geocoder from 'react-map-gl-geocoder'
+// import MapGL from 'react-map-gl'
+// import Geocoder from 'react-map-gl-geocoder'
 
-const MAPBOX_TOKEN = 'pk.eyJ1IjoiYm9yb21lb3QiLCJhIjoiY2t4czN5ZXBuMDQ2ZDJvcGRrdW94YTR2dCJ9.9rCr2ZJ7YUKkm7U3HHEctw';
+// const MAPBOX_TOKEN = 'pk.eyJ1IjoiYm9yb21lb3QiLCJhIjoiY2t4czN5ZXBuMDQ2ZDJvcGRrdW94YTR2dCJ9.9rCr2ZJ7YUKkm7U3HHEctw';
 
 const AddressSection = ({ setFormData }) => {
-  const [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
-  });
-  const mapRef = useRef();
-  const handleViewportChange = useCallback(
-    (newViewport) => setViewport(newViewport),
-    []
-  );
+  // const [viewport, setViewport] = useState({
+  //   latitude: 37.7577,
+  //   longitude: -122.4376,
+  //   zoom: 8
+  // });
+  // const mapRef = useRef();
+  // const handleViewportChange = useCallback(
+  //   (newViewport) => setViewport(newViewport),
+  //   []
+  // );
 
-  const handleGeocoderViewportChange = useCallback(
-    (newViewport) => {
-      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+  // const handleGeocoderViewportChange = useCallback(
+  //   (newViewport) => {
+  //     const geocoderDefaultOverrides = { transitionDuration: 1000 };
 
-      return handleViewportChange({
-        ...newViewport,
-        ...geocoderDefaultOverrides
-      });
-    }, [handleViewportChange]
-  );
+  //     return handleViewportChange({
+  //       ...newViewport,
+  //       ...geocoderDefaultOverrides
+  //     });
+  //   }, [handleViewportChange]
+  // );
 
   return (
     <div id="FMP-target" className="w50p t0 l50 absolute" style={{height: 'calc(100% - 80px)'}}>
-      <MapGL
+      {/* <MapGL
         ref={mapRef}
         {...viewport}
         width="100%"
@@ -70,7 +70,7 @@ const AddressSection = ({ setFormData }) => {
 
           }}
         />
-      </MapGL>
+      </MapGL> */}
     </div>
   );
 };
